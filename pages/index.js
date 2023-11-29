033e039da650efc2353033e5a0998abcd1d4b0c1
Allan <allansimoesadegas11@gmail.com>
2023-11-29 20:24:50 -0300

    Mudando a frase para Impactar a Naju

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Daq um tempinho isso não vai vai mais estar aqui, mas enquanto isso vou registrar que e te amo amor.</h1>
+    return <h1>Daq um tempinho isso não vai vai mais estar aqui, mas enquanto isso vou registrar que e te amo amor</h1>
 }
 
 export default Home;
